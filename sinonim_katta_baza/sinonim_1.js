ec1cbb1b2e91b6c060997767d6b6d4f106dbe5dc
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2025-05-03 04:30:58 +0900

    Add files via upload

---
+++ sinonim_katta_baza/sinonim_1.js
@@ -0,0 +1,8 @@
+const synonyms = {
+  "Salom": ["Assalomu alaykum", "Salomlar"],
+  "Isming nima?": ["Sizning ismingiz nima?", "Siz kimsiz?", "siz kimsiz?", "siz kimsiz"  , "Ismingiz?"],
+  "O‘zbekiston poytaxti qaysi?": ["O‘zbekistonning poytaxti nima?", "O‘zbekiston markazi qayer?", "O‘zbekistonda eng katta shahar?"
+  ,"o'zbekiston poytaxti qaysi?"],
+  "Eng uzun daryo qaysi?": ["Eng uzun suv yo‘li qaysi?", "Qaysi daryo eng katta?"],
+  "Eng baland tog‘ qaysi?": ["Dunyodagi eng baland tog‘?", "Eng katta tog‘?"]
+};
