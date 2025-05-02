043a96a9701f02d5102b2fd6754e7a88373baad6
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2025-05-03 04:30:20 +0900

    Add files via upload

---
+++ katta_baza/baza_1.js
@@ -0,0 +1,8 @@
+// Tayyor savol-javob bazasi
+const knowledgeBase = [
+  { savol: "Salom", javob: "Salom! Qanday yordam bera olaman?" },
+  { savol: "Isming nima?", javob: "Mening ismim Let AI" },
+  { savol: "O‘zbekiston poytaxti qaysi?", javob: "Toshkent." },
+  { savol: "Eng uzun daryo qaysi?", javob: "Nil daryosi." },
+  { savol: "Eng baland tog‘ qaysi?", javob: "Everest." }
+];
