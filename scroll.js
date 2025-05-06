888fa16f1c73437aea808578a071892f2ff764dd
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2025-05-06 10:38:30 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
 const textarea = document.getElementById('user-input');
 
 // Auto height for textarea
-textarea.addEventListener('input', function() {
+textarea.addEventListener('keydown', function() {
   this.style.height = 'auto'; // reset height
   this.style.height = this.scrollHeight + 'px'; // set new height based on content
 });
