7d3caff532a06f1e9f715e61eb2c2effadb59314
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2025-05-06 10:30:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -112,13 +112,7 @@
   }, );
 
 
-const textarea = document.getElementById('user-input');
 
-// Auto height for textarea
-textarea.addEventListener('input', function() {
-  this.style.height = 'auto'; // reset height
-  this.style.height = this.scrollHeight + 'px'; // set new height based on content
-});
 
 
 
