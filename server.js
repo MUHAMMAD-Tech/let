214923ff78b8ce1c8c12a2f5cee5b36bdbd0fc76
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2025-10-25 17:59:02 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["https://your-vercel-domain.vercel.app"],
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", socket => {
+  socket.on("join-room", roomId => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-joined");
+    socket.on("offer", data => socket.to(roomId).emit("offer", data));
+    socket.on("answer", data => socket.to(roomId).emit("answer", data));
+    socket.on("ice-candidate", data => socket.to(roomId).emit("ice-candidate", data));
+    socket.on("chat-message", text => socket.to(roomId).emit("chat-message", { from: socket.id, text }));
+    socket.on("disconnect", () => socket.to(roomId).emit("user-left"));
+  });
+});
+
+server.listen(3000, () => console.log("Signaling server ishga tushdi!"));
